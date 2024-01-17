f78b0fece48f605e0d759617a25db4756d860180
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-01-17 11:49:31 -0300

    Exercício completo

---
+++ src/components/ui/Cabecalho.jsx
@@ -0,0 +1,13 @@
+import Menu from "@/components/ui/Menu"
+
+export default function Cabecalho() {
+    return (
+      <>
+        <header>
+            <h1>PetShop</h1>
+            <Menu />
+        </header>
+      </>
+    )
+  }
+
