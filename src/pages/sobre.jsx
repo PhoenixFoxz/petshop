f766d88e13f66006cdd0d61ab80d1cf72550d554
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-01-19 08:31:22 -0300

    Sobre

--- src/pages/sobre.jsx
+++ src/pages/sobre.jsx
@@ -15,6 +15,24 @@ export default function Sobre() {
       {/* //Antes era <section> mudamos por causa do css */}
       <StyledSobre>
         <h2>Sobre nossos PetShop</h2>
+
+        <h3>Missão</h3>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque eos, vero quod ullam quam voluptates quasi ea pariatur odio maxime, suscipit corrupti asperiores sit nesciunt nulla accusamus. Vero, commodi nostrum?
+        </p>
+
+        <h3>Visão</h3>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione veniam odit obcaecati modi nam nulla sapiente aspernatur quae? Quibusdam corporis odio quidem nulla vel accusantium numquam! Facere culpa dolorum ullam iusto quo, nemo repellendus nobis minus quas eligendi voluptates sunt vero deleniti, error illo fugiat. Fugiat, maiores possimus? Itaque reprehenderit fugiat facere eius deleniti eum, exercitationem fugit nesciunt molestiae repudiandae natus et. Fugit aperiam ab vero doloribus? Quasi, nulla praesentium.
+        </p>
+
+        <h3>Valores</h3>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda exercitationem id placeat ex. Labore nihil maxime expedita odio, quam dolorum.
+        </p>
       </StyledSobre>
     </>
   );
