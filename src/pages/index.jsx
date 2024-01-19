e7d92ba6b5076973adb39516c549c8aaf3dbd0e1
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-01-19 10:33:55 -0300

    Formatação do article

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import styled from "styled-components";
 
 export default function Home() {
@@ -19,10 +20,49 @@ export default function Home() {
       {/* //Antes era <section> mudamos por causa do css */}
       <StyledHome>
         <h2>Pet Notícias</h2>
+        <StyledListaPosts>
+          <article>
+            <Link href="/">
+              <h3>Título do post...</h3>
+              <p>Subtítulo do post...</p>
+            </Link>
+          </article>
+          <article>
+            <Link href="/">
+              <h3>Título do post...</h3>
+              <p>Subtítulo do post...</p>
+            </Link>
+          </article>
+        </StyledListaPosts>
       </StyledHome>
     </>
   );
 }
+
+const StyledListaPosts = styled.div`
+  article {
+    background-color: #f7f7f7;
+    padding: 1rem;
+    margin-bottom: 1rem;
+    box-shadow: var(--sombra-box);
+    border-radius: var(--borda-arredondada);
+    transition: transform 200ms;
+  }
+
+  article:hover {
+    cursor: pointer;
+    transform: scale(1.05);
+  }
+
+  & a {
+    text-decoration: none;
+    color: black;
+
+    &:hover, &:focus {
+    color: #0066ff;
+  }
+  }
+`;
 
 const StyledHome = styled.section`
   h2::before {
